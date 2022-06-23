82313e28bb325854532dca34e7e4a316e8278880
jamaps <jeffallen89@gmail.com>
2022-06-23 14:51:06 -0400

    top bar, body base styles

--- src/App.svelte
+++ src/App.svelte
@@ -9,5 +9,18 @@
 </main>
 
 <style>
-
+	@font-face {
+		font-family: TradeGothicBold;
+		src: url("./assets/Trade Gothic LT Bold.ttf");
+	}
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+			Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+	}
+	:global(body) {
+		padding: 0px;
+		margin: 0px;
+		background-color: rgb(255, 255, 255);
+	}
+	
 </style>
--- src/lib/TopSofC.svelte
+++ src/lib/TopSofC.svelte
@@ -11,16 +11,16 @@
 		position: fixed;
    		top: 0px;
 		height: 50px;
-		background-color: #000000;
+		background-color: #ffffff;
 		margin-bottom: 20px;
-		border-bottom: 1px solid rgb(38, 38, 38);
+		border-bottom: 1px solid rgb(235, 235, 235);
 		width: 100%;
 		min-width: 200px;
 		padding-left: 0px;
 		padding-right: 0px;
 		text-align: center;
 		z-index: 5;
-		box-shadow: 0 3px 0 0 rgba(45, 45, 45, 0.268);
+		box-shadow: 0 3px 0 0 rgba(227, 227, 227, 0.268);
 	}
 	img {
 		color: blue;
