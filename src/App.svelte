fbbcfe2875b011a98471f1a3891a2d03cd48a384
jamaps <jeffallen89@gmail.com>
2022-06-24 13:22:09 -0400

    dif size maps

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,26 @@
 
 </script>
 
+
+<Top />
 <main>
-	<Top />
-	<Map />
+	
+	<div class="title">
+		<h1>Hello</h1>
+	</div>
+	<div class="mapBig">
+		<Map />
+	</div>
+	<div class="text">
+		<p>Hello</p>
+	</div>
+	<div class="mapSmall">
+		<b>1961</b>
+		<Map />
+	</div>
+
 </main>
+
 
 <style>
 	@font-face {
@@ -23,6 +39,35 @@
 		padding: 0px;
 		margin: 0px;
 		background-color: rgb(255, 255, 255);
+	}
+
+	main {
+		margin: 0 auto;
+		width: 100%;
+		position: relative;
+	}
+
+	.title {
+		margin: auto;
+		max-width: 600px;
+		width: 100%;
+		margin-top: 70px;
+	}
+
+	.text {
+		margin: auto;
+		max-width: 600px;
+		width: 100%;
+	}
+
+	.mapBig {
+		margin: auto;
+		max-width: 800px;
+	}
+
+	.mapSmall {
+		margin: auto;
+		max-width: 400px;
 	}
 	
 </style>
--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,52 +1,74 @@
-<script>
+<script>	
 	import inc1961 from "../data/1961.geo.json";
-	import formerMun from "../data/formerMun5.geo.json";
+	import formerMun from "../data/formerMun.geo.json";
 	import { geoPath, geoMercator, scaleThreshold } from "d3";
 
 	export const year = "1961";
 
 	const avgInc = {
-		"1961": 4306.65
-	}
+		"1961": 4306.65,
+	};
 
-	const width = 800,
-		height = 550;
+	let divWidth = 800;
+	$: innerWidth = divWidth;
+	$: height = innerWidth * 40/80
 
-	const projection = geoMercator()
-		.center([-79.31, 43.76])
-		.scale([75000])
+	let c1 = [-79.29, 43.70]
+	let c2 = [-78.89, 43.64]
+
+	$: console.log(innerWidth)
+	$: console.log([-0.004* innerWidth - 81.49, 0.00015 * innerWidth + 43.58 ])
+
+	$: projection = geoMercator()
+		.center([-0.001* innerWidth - 78.49, 0.00015 * innerWidth + 43.58 ])
+		.scale([75000 * innerWidth / 800])
 		.angle([-17]);
-	const path = geoPath(projection);
+	$: path = geoPath(projection);	
 
 	let tracts = inc1961.features;
 
 	var color = scaleThreshold()
-    .domain([avgInc[year] * 0.6, avgInc[year] * 0.8, avgInc[year] * 1.2, avgInc[year] * 1.4	])
-    .range(['#DC4633','#e89a91','#e6e3d6','#bed9dd','#6FC7EA']);
+		.domain([
+			avgInc[year] * 0.6,
+			avgInc[year] * 0.8,
+			avgInc[year] * 1.2,
+			avgInc[year] * 1.4,
+		])
+		.range(["#DC4633", "#e89a91", "#e6e3d6", "#bed9dd", "#6FC7EA"]);
 
-	['#DC4633','#e89a91','#e6e3d6','#7bccc0','#00a189']
-
-	
-	tracts.map(item => {item.properties.avg_inc ? item.properties.color = color(item.properties.avg_inc) : item.properties.color = "white"});
-
+	tracts.map((item) => {
+		item.properties.avg_inc
+			? (item.properties.color = color(item.properties.avg_inc))
+			: (item.properties.color = "white");
+	});
 </script>
 
-	<svg {width} {height} id="meow">
+<div id="container" class="svg-container" bind:offsetWidth={divWidth}>
+	<svg width={innerWidth} height={height} id="meow">
 		{#each formerMun.features as data}
-			<path id="fm-back" d={path(data)}/>
+			<path id="fm-back" d={path(data)} />
 		{/each}
 		{#each tracts as data}
-			<path id="ct" d={path(data)} fill={data.properties.color}	/>
+			<path id="ct" d={path(data)} fill={data.properties.color} />
 		{/each}
 		{#each formerMun.features as data}
-			<path id="fm" d={path(data)}/>
+			<path id="fm" d={path(data)} />
 		{/each}
 	</svg>
+</div>
 
 <style>
-	
+	#container {
+		/* margin: 0 auto; */
+		/* padding-left: 0px; */
+		width: 100%;
+		max-width: 800px;
+		min-width: 400px;
+	}
 	#meow {
-		border: solid 1px black;
+		border: solid 1px rgb(217, 217, 217);
+		/* border-radius: 5px; */
+		border-bottom-right-radius: 100px;
 	}
 
 	#fm-back {
@@ -60,8 +82,8 @@
 		stroke-width: 1px;
 	}
 	#fm {
-		stroke: rgb(59, 59, 59);
-		stroke-width: 1	px;
+		stroke: rgb(107, 107, 107);
+		stroke-width: 1 px;
 		fill-opacity: 0;
 	}
 </style>
