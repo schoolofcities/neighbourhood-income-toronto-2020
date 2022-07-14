30e64f7a6981faa3c6071e07dcb0c0e8d40f2190
jamaps <jeffallen89@gmail.com>
2022-07-14 14:34:56 -0400

    url edit

--- src/App.svelte
+++ src/App.svelte
@@ -53,7 +53,7 @@
 	<meta name="twitter:site" content="@jeffallenmaps" />
 	<meta name="twitter:title" content="Wealth inequalities and poverty concentration in Toronto" />
 	<meta name="twitter:description" content="Our first look at 2020 annual income data collected as part of the 2021 Canadian census" />
-	<meta name="twitter:image" content="./assets/TopGraphic.png" />
+	<meta name="twitter:image" content="https://raw.githubusercontent.com/schoolofcities/neighbourhood-income-toronto-2020/main/img/TopGraphic.png" />
 
 </svelte:head>
 
