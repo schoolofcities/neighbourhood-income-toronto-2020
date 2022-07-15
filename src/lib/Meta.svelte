eb2c138c01b0a9e58073a85ff8dc3429262a4336
jamaps <jeffallen89@gmail.com>
2022-07-15 09:21:14 -0400

    meta in

--- src/lib/Meta.svelte
+++ src/lib/Meta.svelte
@@ -2,7 +2,7 @@
 
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="https://schoolofcities.github.io/" />
-<meta name="twitter:creator" content="@jeffallenmaps" />
+<meta name="twitter:creator" content="@JeffAllenMaps" />
 <meta name="twitter:title" content="Wealth inequalities and poverty concentration in Toronto" />
 <meta name="twitter:description" content="Our first look at 2020 annual income data collected as part of the 2021 Canadian census" />
 <meta name="twitter:image" content="https://raw.githubusercontent.com/schoolofcities/neighbourhood-income-toronto-2020/main/img/TopGraphic.png" />
